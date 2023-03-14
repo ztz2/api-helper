2294748cdcaabd2d85f452f74211a2f2ee4b4c80
ztz2 <cn80393313@gmail.com>
2023-03-14 21:12:38 +0800

    feat: 文档更新

--- packages/docs/.vitepress/config.ts
+++ packages/docs/.vitepress/config.ts
@@ -10,165 +10,29 @@ const nav: ThemeConfig['nav'] = [
   { text: 'CLI', link: '/config/cli', activeMatch: `^/config/cli/`}
 ]
 
-export const sidebar: ThemeConfig['sidebar'] = {
-  '/guide': [
-    {
-      text: '准备开始',
-      items: [
-        { text: '指南', link: '/guide/introduction' },
-        {
-          text: '快速开始',
-          link: '/guide/quick-start'
-        }
-      ]
-    },
-    {
-      text: '配置',
-      items: [
-        { text: 'CLI', link: '/config/cli' },
-        { text: '模板功能', link: '/config/template' },
-      ]
-    }
-  ],
-  '/config': [
-    {
-      text: '配置',
-      items: [
-        { text: 'CLI', link: '/config/cli' },
-        { text: '模板功能', link: '/config/template' },
-      ]
-    }
-  ],
+const sidebarConfig = [
+  {
+    text: '准备开始',
+    items: [
+      { text: '指南', link: '/guide/introduction' },
+      {
+        text: '快速开始',
+        link: '/guide/quick-start'
+      }
+    ]
+  },
+  {
+    text: '配置',
+    items: [
+      { text: 'CLI', link: '/config/cli' },
+      { text: '模板功能', link: '/config/template' },
+    ]
+  }
+]
 
-  // '/examples/': [
-  //   {
-  //     text: 'Basic',
-  //     items: [
-  //       {
-  //         text: 'Hello World',
-  //         link: '/examples/#hello-world'
-  //       },
-  //       {
-  //         text: 'Handling User Input',
-  //         link: '/examples/#handling-input'
-  //       },
-  //       {
-  //         text: 'Attribute Bindings',
-  //         link: '/examples/#attribute-bindings'
-  //       },
-  //       {
-  //         text: 'Conditionals and Loops',
-  //         link: '/examples/#conditionals-and-loops'
-  //       },
-  //       {
-  //         text: 'Form Bindings',
-  //         link: '/examples/#form-bindings'
-  //       },
-  //       {
-  //         text: 'Simple Component',
-  //         link: '/examples/#simple-component'
-  //       }
-  //     ]
-  //   },
-  //   {
-  //     text: 'Practical',
-  //     items: [
-  //       {
-  //         text: 'Markdown Editor',
-  //         link: '/examples/#markdown'
-  //       },
-  //       {
-  //         text: 'Fetching Data',
-  //         link: '/examples/#fetching-data'
-  //       },
-  //       {
-  //         text: 'Grid with Sort and Filter',
-  //         link: '/examples/#grid'
-  //       },
-  //       {
-  //         text: 'Tree View',
-  //         link: '/examples/#tree'
-  //       },
-  //       {
-  //         text: 'SVG Graph',
-  //         link: '/examples/#svg'
-  //       },
-  //       {
-  //         text: 'Modal with Transitions',
-  //         link: '/examples/#modal'
-  //       },
-  //       {
-  //         text: 'List with Transitions',
-  //         link: '/examples/#list-transition'
-  //       },
-  //       {
-  //         text: 'TodoMVC',
-  //         link: '/examples/#todomvc'
-  //       }
-  //     ]
-  //   },
-  //   {
-  //     // https://eugenkiss.github.io/7guis/
-  //     text: '7 GUIs',
-  //     items: [
-  //       {
-  //         text: 'Counter',
-  //         link: '/examples/#counter'
-  //       },
-  //       {
-  //         text: 'Temperature Converter',
-  //         link: '/examples/#temperature-converter'
-  //       },
-  //       {
-  //         text: 'Flight Booker',
-  //         link: '/examples/#flight-booker'
-  //       },
-  //       {
-  //         text: 'Timer',
-  //         link: '/examples/#timer'
-  //       },
-  //       {
-  //         text: 'CRUD',
-  //         link: '/examples/#crud'
-  //       },
-  //       {
-  //         text: 'Circle Drawer',
-  //         link: '/examples/#circle-drawer'
-  //       },
-  //       {
-  //         text: 'Cells',
-  //         link: '/examples/#cells'
-  //       }
-  //     ]
-  //   }
-  // ],
-  // '/style-guide/': [
-  //   {
-  //     text: 'Style 指南',
-  //     items: [
-  //       {
-  //         text: 'Overview',
-  //         link: '/style-guide/'
-  //       },
-  //       {
-  //         text: 'A - Essential',
-  //         link: '/style-guide/rules-essential'
-  //       },
-  //       {
-  //         text: 'B - Strongly Recommended',
-  //         link: '/style-guide/rules-strongly-recommended'
-  //       },
-  //       {
-  //         text: 'C - Recommended',
-  //         link: '/style-guide/rules-recommended'
-  //       },
-  //       {
-  //         text: 'D - Use with Caution',
-  //         link: '/style-guide/rules-use-with-caution'
-  //       }
-  //     ]
-  //   }
-  // ]
+export const sidebar: ThemeConfig['sidebar'] = {
+  '/guide': sidebarConfig,
+  '/config': sidebarConfig
 }
 
 // Placeholder of the i18n config for @vuejs-translations.
