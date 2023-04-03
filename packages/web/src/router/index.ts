c9ba977cb54d674d0de1654d2e20bf81cb0c3e65
ztz2 <cn80393313@gmail.com>
2023-04-04 01:12:51 +0800

    feat: 路由加载进度

--- packages/web/src/router/index.ts
+++ packages/web/src/router/index.ts
@@ -3,7 +3,12 @@ import {
   createWebHashHistory,
   RouteRecordRaw,
 } from 'vue-router';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
+
 import Home from '@/views/home/index.vue';
+
+NProgress.configure({ showSpinner: false });
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -24,6 +29,13 @@ const routes: Array<RouteRecordRaw> = [
 const router = createRouter({
   history: createWebHashHistory(),
   routes,
+});
+router.beforeEach((to, from, next) => {
+  NProgress.start();
+  next();
+});
+router.afterEach(() => {
+  NProgress.done();
 });
 
 export default router;
