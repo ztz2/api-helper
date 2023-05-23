fe7f7ec11d21e3f11a4302d4402f19a30da7b90f
ztz2 <cn80393313@gmail.com>
2023-05-23 16:21:06 +0800

    Update jest.config.js

--- packages/core/jest.config.js
+++ packages/core/jest.config.js
@@ -1,14 +1,15 @@
 module.exports = {
   globals: {},
-  preset: "ts-jest",
+  preset: 'ts-jest',
   transform: {
-    "^.+\.(jsx|tsx|ts|cjs|js|html)$": "ts-jest",
+    '^.+\.(jsx|tsx|ts|cjs|js|html)$': 'ts-jest',
   },
   collectCoverageFrom: [
-    "**/*.test.{js,jsx,tsx,ts}",
-    "**/*__test__/*.{js,jsx,tsx,ts}"
+    '**/*.test.{js,jsx,tsx,ts}',
+    '**/*__test__/*.{js,jsx,tsx,ts}'
   ],
   cacheDirectory: 'node_modules/.cache/jest',
-  // 下面非要从重要, 将不忽略 lodash-es, other-es-lib 这些es库, 从而使ts-jest去处理它们
-  transformIgnorePatterns: ["<rootDir>/node_modules/(?!(axios))"]
+  transformIgnorePatterns: [
+    '\\/node_modules\\/(?!(lodash-es|ora\\/)\\/)'
+  ]
 }
