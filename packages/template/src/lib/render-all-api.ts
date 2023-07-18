a9d3cbf08bbec03b05393f8d9952323503c03b7f
ztz2 <cn80393313@gmail.com>
2023-07-18 15:03:42 +0800

    feat: 增加自定义interface和API名称

--- packages/template/src/lib/render-all-api.ts
+++ packages/template/src/lib/render-all-api.ts
@@ -1,14 +1,16 @@
 import { getSchema } from '@api-helper/core/lib/helpers';
 import { APIHelper } from '@api-helper/core/lib/types';
 import formatCode from '@/lib/utils/prettier';
-import { renderInterface } from '@/lib/render-interface';
-import { renderRequestFunction } from '@/lib/render-request-function';
+import { renderInterface, renderInterfaceName } from '@/lib/render-interface';
+import { renderRequestFunction, renderRequestFunctionName } from '@/lib/render-request-function';
 
 export function renderAllApi(
   apiDocument: APIHelper.Document,
   options?: {
     codeType: 'typescript' | 'javascript';
     dataKey?: string;
+    onRenderInterfaceName?: typeof renderInterfaceName,
+    onRenderRequestFunctionName?: typeof renderRequestFunctionName,
   }
 ) {
   if (!apiDocument) {
@@ -34,14 +36,29 @@ export function renderAllApi(
         responseDataSchema = getSchema(api.responseDataSchema, dataKey)
       }
       // 1. 生成interface-请求数据
-      p.push(renderInterface(api.requestDataSchema, api, { paramType: 'request' }));
+      p.push(renderInterface(api.requestDataSchema, api, {
+        paramType: 'request',
+        onRenderInterfaceName: options?.onRenderInterfaceName,
+      }));
       // 2. 生成interface-请求数据（特殊不兼容数据类型）
-      p.push(renderInterface(api.requestExtraDataSchema, api, { paramType: 'request', isExtraData: true }));
+      p.push(renderInterface(api.requestExtraDataSchema, api, {
+        paramType: 'request',
+        isExtraData: true,
+        onRenderInterfaceName: options?.onRenderInterfaceName,
+      }));
       // 2. 生成interface-响应数据
-      p.push(renderInterface(responseDataSchema, api, { paramType: 'response' }));
+      p.push(renderInterface(responseDataSchema, api, {
+        paramType: 'response',
+        onRenderInterfaceName: options?.onRenderInterfaceName,
+      }));
     }
     // 3. 生成请求函数
-    p.push(renderRequestFunction(api, { codeType, dataKey }));
+    p.push(renderRequestFunction(api, {
+      codeType,
+      dataKey,
+      onRenderInterfaceName: options?.onRenderInterfaceName,
+      onRenderRequestFunctionName: options?.onRenderRequestFunctionName,
+    }));
 
     code.push(p.join(''));
   }
