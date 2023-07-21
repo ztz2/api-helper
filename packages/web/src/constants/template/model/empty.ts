a95a570f83f1dac9ac158c959e191c810645ab1c
ztz2 <cn80393313@gmail.com>
2023-07-21 13:54:27 +0800

    feat: 增加过滤原始值的Schema工具函数。

--- packages/web/src/constants/template/model/empty.ts
+++ packages/web/src/constants/template/model/empty.ts
@@ -5,8 +5,11 @@ export default function genEmptyModelTemplate() {
   return new Template({
     content: `${COMMON_HEAD}
   const api = params.api;
-  const requestDataSchemaList = params.requestDataSchemaList;
-  const responseDataSchemaList = params.responseDataSchemaList;
+  // 过滤原始值的Schema。保留纯粹的类型对象。原始值Schema用于TS类型申明有用，在生成JS对象，Class实体类时候，这些原始值类型则无用，需要过滤掉。
+  const requestDataSchemaList = apih.core.filterSchemaPrimitiveValue(params.requestDataSchemaList);
+  // 过滤原始值的Schema。保留纯粹的类型对象。原始值Schema用于TS类型申明有用，在生成JS对象，Class实体类时候，这些原始值类型则无用，需要过滤掉。
+  const responseDataSchemaList = apih.core.filterSchemaPrimitiveValue(params.responseDataSchemaList);
+
   const tpl1 = \`生成模板例子，这里获取请求数据和响应数据的字段，根据这些字段，可以生成想要的任何模板代码
 
 《if requestDataSchemaList.length > 0》
